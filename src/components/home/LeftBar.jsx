0f58c233ce261b2c8c66202d450917a2039a3491
mvandertuig <mayah90@gmail.com>
2021-01-21 12:26:13 -0800

    Fonts

--- src/components/home/LeftBar.jsx
+++ src/components/home/LeftBar.jsx
@@ -5,8 +5,9 @@ const LeftBar = () => {
     return (
         <div className={'leftBar sidebar'}>
             <Row className={'justify-content-center'}>
-                <i className="fab fa-medium-m fa-2x"></i>
+                <i className="fas fa-home fa-2x"></i>
             </Row>
+
         </div>
 
     );
--- src/components/home/MainBody.jsx
+++ src/components/home/MainBody.jsx
@@ -29,30 +29,26 @@ const MainBody = () => {
 
   return (
     <div>
-      <div
-        id="home"
-        className="title jumbotron jumbotron-fluid bg-transparent bgstyle text-light min-vh-100 d-flex align-content-center align-items-center flex-wrap m-0"
+      <div id={'home'} className="title jumbotron jumbotron-fluid bg-transparent bgstyle text-light min-vh-100 d-flex align-content-center align-items-center flex-wrap m-0"
       >
         <div id="stars" className={'stars'}></div>
         <div className={'container container-fluid text-center'}>
           <Row className={'justify-content-center'}>
-            <h1 className="display-1">
-              Mayah Vandertuig
+            <h1 className="display-1"><b>Mayah Vandertuig</b>
             </h1>
           </Row>
           <Row className={'justify-content-center'}>
-            <Typist className="lead"> {devDesc}</Typist>
+            <Typist className="lead">Software Engineer | Web Developer | Artist</Typist>
           </Row>
-
+          <a
+              className=" btn btn-lg explore"
+              href="#aboutme"
+          >
+            <i className="fas fa-long-arrow-alt-down fa-2x"/>
+          </a>
         </div>
-        {/*<div className={'explore justify-content-center'}>*/}
-        {/*  <a*/}
-        {/*      className=" btn btn-lg"*/}
-        {/*      href="#aboutme"*/}
-        {/*  >*/}
-        {/*    <i className="fas fa-angle-double-down fa-lg"/>*/}
-        {/*  </a>*/}
-        {/*</div>*/}
+
+
       </div>
     </div>
   );
